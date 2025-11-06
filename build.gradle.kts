2d0cdf54c3724ba5eb4af4f7cfe1a990105bb083
XoorFrak-oss <marcfastrez049@gmail.com>
2025-11-06 22:31:57 +0100

    Remove plugin management section from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
-// Racine du projet Gradle
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
