f72a778ef27a24c39e8f59d72cb7d2969766b9b2
XoorFrak-oss <marcfastrez049@gmail.com>
2025-11-06 22:28:49 +0100

    Add plugin management to build.gradle.kts
    Added plugin management section with repositories.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+// Racine du projet Gradle
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
